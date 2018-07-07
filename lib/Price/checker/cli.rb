74d6ca8838b28e51d99da65a28481a9b1c98bf5e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-07 15:16:57 +0000

    first

---
+++ lib/Price/checker/cli.rb
@@ -0,0 +1,9 @@
+require "Price/checker/cli/version"
+
+module Price
+  module Checker
+    module Cli
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/Price/checker/cli/version.rb
@@ -0,0 +1,7 @@
+module Price
+  module Checker
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end

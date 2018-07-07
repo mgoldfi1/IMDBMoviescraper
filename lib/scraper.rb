c50d1a90b316ee79f103d1bece72e3861188ebb1
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-07 17:31:19 +0000

    url_scraper testing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-class Scraper
+#class Scraper
 
 def url_generator(user_input)
 keywords = user_input.split(" ")
@@ -11,6 +11,26 @@ default = "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dpets&field
   end
   default
 end
+
+
+def url_scraper
+search = Nokogiri::HTML(open(url_generator(user_input)))
+hash_array = []
+count = 0
+until count == 10
+  search.css("ul li").each do |item|
+    search_hash = {}
+    search_hash[:price] = item.css("a span.a-offscreen").text.gsub("$","")
+    search_hash[:name] =  item.css("h2 span.a-offscreen").text
+    hash_array << search_hash
+    count += 1
+  end
+  hash_array
+end
+
+
+
+
 
 
 
